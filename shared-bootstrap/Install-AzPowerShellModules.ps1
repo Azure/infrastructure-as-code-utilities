1f838447298e707cca2499f51d85d402c27c1fc7
Jared Holgate <jaredholgate@microsoft.com>
2025-01-28 13:08:31 +0000

    Skip PS install prompt

--- shared-bootstrap/Install-AzPowerShellModules.ps1
+++ shared-bootstrap/Install-AzPowerShellModules.ps1
@@ -4,5 +4,5 @@ param(
 )
 
 Write-Output "- Installing Az PowerShell Module..."
-Install-Module Az -SkipPublisherCheck
+Install-Module Az -SkipPublisherCheck -Force
 Write-Output "- Finished Installing Az PowerShell Module..."
