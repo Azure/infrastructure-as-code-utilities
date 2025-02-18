bab8e9852d5f160e0586b50f0bb5c3a8c86f4820
Jared Holgate <jaredholgate@microsoft.com>
2025-02-18 12:20:23 +0000

    Update Install-BicepCliForWindows.ps1

--- shared-bootstrap/Install-BicepCliForWindows.ps1
+++ shared-bootstrap/Install-BicepCliForWindows.ps1
@@ -11,16 +11,18 @@ if($statusCode -ne 200) {
   throw "Unable to query git repository version..."
 }
 
-Write-Output "- Downloading Bicep..."
 $version = $releaseData.tag_name
-$exeName = "bicep-win-x64.exe"
+$asset = $releaseData.assets | Where-Object { $_.name.EndsWith("bicep-win-x64.exe")  }
+$url = $asset.browser_download_url
+Write-Output "- Found Bicep Version $version"
+
+Write-Output "- Downloading Bicep..."
 $targetExeName = "bicep.exe"
 $installPath = Join-Path -Path $toolsPath -ChildPath "bicep_$version"
 New-Item -Path $installPath -ItemType Directory -Force | Out-String | Write-Verbose
 
-
 $targetFile = Join-Path -Path $installPath -ChildPath $targetExeName
-$url = "https://github.com/Azure/bicep/releases/download/$version/$exeName"
+Write-Output "- Downloading from $url"
 Invoke-WebRequest -Uri $url -OutFile "$targetFile"
 
 ## Set Env vars
