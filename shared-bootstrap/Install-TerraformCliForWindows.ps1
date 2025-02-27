3cfdd70c4cf9139999f07490f6ee2fc1a3976c83
Jared Holgate <jaredholgate@microsoft.com>
2025-02-27 16:39:35 +0000

    Fix Terraform install due to HashiCorp API change

--- shared-bootstrap/Install-TerraformCliForWindows.ps1
+++ shared-bootstrap/Install-TerraformCliForWindows.ps1
@@ -5,17 +5,21 @@ param(
 
 ## Get latest version of Terraform
 Write-Output "- Finding Latest Terraform Version..."
-$versionResponse = Invoke-WebRequest -Uri "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+$versionResponse = Invoke-WebRequest -Uri "https://api.releases.hashicorp.com/v1/releases/terraform?limit=20"
 if($versionResponse.StatusCode -ne "200") {
     throw "Unable to query Terraform version, please check your internet connection and try again..."
 }
-$version = ($versionResponse).Content | ConvertFrom-Json | Select-Object -ExpandProperty current_version
+$releases = ($versionResponse).Content | ConvertFrom-Json | Where-Object -Property is_prerelease -EQ $false
+$release = $releases[0]
+$version = $release.version
 
 ## Download Terraform
 Write-Output "- Downloading Terraform $version..."
 $unzipdir = Join-Path -Path $toolsPath -ChildPath "terraform_$version"
 $zipfilePath = "$unzipdir.zip"
-$url = "https://releases.hashicorp.com/terraform/$($version)/terraform_$($version)_windows_amd64.zip" 
+$os = "windows"
+$architecture = "amd64"
+$url = $release.builds | Where-Object { $_.arch -eq $architecture -and $_.os -eq $os } | Select-Object -First 1 -ExpandProperty url
 Invoke-WebRequest -Uri $url -OutFile "$zipfilePath"
 
 ## Unzip Terraform
