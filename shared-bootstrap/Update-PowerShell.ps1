c20d821caec20124aa753acbb9898f4a8563830f
Jared Holgate <jaredholgate@microsoft.com>
2025-12-16 08:20:54 +0000

    attempt seamless update

--- shared-bootstrap/Update-PowerShell.ps1
+++ shared-bootstrap/Update-PowerShell.ps1
@@ -7,9 +7,13 @@ Write-Output "- Updating PowerShell..."
 $result = winget list --id Microsoft.Powershell
 if($result -eq "No installed package found matching input criteria.") {
     Write-Output "- Found no existing winget PowerShell installation, installing latest version..."
+    cmd
     winget install --id Microsoft.Powershell
+    pwsh
 } else {
     Write-Output "- Found existing winget PowerShell installation, updating to latest version..."
+    cmd
     winget upgrade --id Microsoft.Powershell
+    pwsh
 }
 Write-Output "- Finished Updating PowerShell..."
