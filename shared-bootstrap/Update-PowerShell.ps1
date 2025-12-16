408b916ffbd45d9ec5a038e44fa0df8810fdc311
Jared Holgate <jaredholgate@microsoft.com>
2025-12-16 08:24:23 +0000

    debug

--- shared-bootstrap/Update-PowerShell.ps1
+++ shared-bootstrap/Update-PowerShell.ps1
@@ -7,13 +7,10 @@ Write-Output "- Updating PowerShell..."
 $result = winget list --id Microsoft.Powershell
 if($result -eq "No installed package found matching input criteria.") {
     Write-Output "- Found no existing winget PowerShell installation, installing latest version..."
-    cmd
     winget install --id Microsoft.Powershell
-    pwsh
 } else {
     Write-Output "- Found existing winget PowerShell installation, updating to latest version..."
-    cmd
     winget upgrade --id Microsoft.Powershell
-    pwsh
+    Write-Debug "- winget upgrade result: $LASTEXITCODE"
 }
 Write-Output "- Finished Updating PowerShell..."
