57d1b004108aac68b21d3f684502f616f20e3207
Jared Holgate <jaredholgate@microsoft.com>
2025-12-16 08:42:46 +0000

    add some colour

--- shared-bootstrap/Update-PowerShell.ps1
+++ shared-bootstrap/Update-PowerShell.ps1
@@ -5,7 +5,7 @@ param(
 
 Write-Output "- Updating PowerShell..."
 $result = winget list --id Microsoft.Powershell
-Write-Ouput "- IMPORTANT! It is possible that updating PowerShell may cause this script to terminate early. If this happens, please close the terminal window, open a new admin terminal and run the script again..."
+Write-Host "- IMPORTANT! It is possible that updating PowerShell may cause this script to terminate early. If this happens, please close the terminal window, open a new admin terminal and run the script again..." -ForegroundColor Yellow
 if($result -eq "No installed package found matching input criteria.") {
     Write-Output "- Found no existing winget PowerShell installation, installing latest version..."
     winget install --id Microsoft.Powershell
