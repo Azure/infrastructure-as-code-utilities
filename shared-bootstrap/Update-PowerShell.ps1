384b410c5baa79b3a3d8b8be68a01d8ac47bb829
Jared Holgate <jaredholgate@microsoft.com>
2025-12-16 08:41:35 +0000

    add message

--- shared-bootstrap/Update-PowerShell.ps1
+++ shared-bootstrap/Update-PowerShell.ps1
@@ -5,12 +5,12 @@ param(
 
 Write-Output "- Updating PowerShell..."
 $result = winget list --id Microsoft.Powershell
+Write-Ouput "- IMPORTANT! It is possible that updating PowerShell may cause this script to terminate early. If this happens, please close the terminal window, open a new admin terminal and run the script again..."
 if($result -eq "No installed package found matching input criteria.") {
     Write-Output "- Found no existing winget PowerShell installation, installing latest version..."
     winget install --id Microsoft.Powershell
 } else {
     Write-Output "- Found existing winget PowerShell installation, updating to latest version..."
     winget upgrade --id Microsoft.Powershell
-    Write-Debug "- winget upgrade result: $LASTEXITCODE"
 }
 Write-Output "- Finished Updating PowerShell..."
